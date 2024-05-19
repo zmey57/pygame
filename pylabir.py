d018d2ae22c7255e4cdd4a4287a55a919f1d4d5c
zmey57 <145285270+zmey57@users.noreply.github.com>
2024-05-19 18:39:36 +0300

    Update pylabir.py

--- pylabir.py
+++ pylabir.py
@@ -1,19 +1,19 @@
 # This is the main file.
 import pygame as pg
 pg.init()
-screen = pg.display.set_mode([500, 500])
-screen.fill([255, 255, 255])
+screen = pg.display.set_mode((500, 500))
+bg = pg.Surface(screen.get_size())
+bg.fill((255, 255, 255))
 kitten = pg.image.load('cutecat.png')
-wall = pg.image.load('wall.png')
-wall2 = pg.image.load('wall2.png')
+wallimage = pg.image.load('wall.png')
+wall2image = pg.image.load('wall2.png')
 
 
 class Player(pg.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.surf = kitten
-        self.rect = self.surf.get_rect()
-        screen.blit(self.surf, self.rect)
+        self.rect = [10, 10]
 
 
 class Wall(pg.sprite.Sprite):
@@ -21,16 +21,19 @@ class Wall(pg.sprite.Sprite):
         super(Wall, self).__init__()
         self.rotate = rotate
         if self.rotate == 'ver':
-            self.surf = wall2
+            self.surf = wall2image
         elif self.rotate == 'hor':
-            self.surf = wall
+            self.surf = wallimage
         self.rect = rect
-        screen.blit(self.surf, self.rect)
 
 
 player = Player()
-wall = Wall((250, 250), 'hor')
-
+wall = Wall((0, -5), 'hor')
+wall2 = Wall((100, 100), 'hor')
+wall3 = Wall((0, 20), 'hor')
+wlist = [wall, wall2, wall3]
+screen.blit(player.surf, player.rect)
+screen.blit(wall.surf, wall.rect)
 pg.display.flip()
 r = True
 while r:
@@ -39,12 +42,20 @@ while r:
             r = False
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_UP:
-                player.rect.move_ip(0, 5)
+                if 0 < player.rect[1] < 500 and 0 < player.rect[0] < 500:
+                    player.rect[1] -= 5
             if event.key == pg.K_DOWN:
-                player.rect.move_ip(0, -5)
+                if 0 < player.rect[1] < 500 and 0 < player.rect[0] < 500:
+                    player.rect[1] += 5
             if event.key == pg.K_RIGHT:
-                player.rect.move_ip(5, 0)
+                if 0 < player.rect[1] < 500 and 0 < player.rect[0] < 500:
+                    player.rect[0] += 5
             if event.key == pg.K_LEFT:
-                player.rect.move_ip(-5, 0)
+                if 0 < player.rect[1] < 500 and 0 < player.rect[0] < 500:
+                    player.rect[0] -= 5
+    screen.blit(bg, (0, 0))
+    screen.blit(player.surf, player.rect)
+    for i in range(len(wlist)):
+        screen.blit(wlist[i].surf, wlist[i].rect)
     pg.display.update()
 pg.quit()
